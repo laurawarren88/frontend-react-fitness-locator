a8553676436be1c59474045a9316cf3d4d654378
laurawarren88 <laura@enterpriseautomation.co.uk>
2025-01-09 10:54:41 +0000

    Added new fields to the model

--- src/models/activitiesModel.jsx
+++ src/models/activitiesModel.jsx
@@ -1,18 +1,23 @@
 /**
- * @typedef {Object} Place
- * @property {number} id
- * @property {string} name 
- * @property {string} address
- * @property {string} city 
- * @property {string} postcode 
- * @property {string} description
- * @property {number} rating
- * @property {string} phone
- * @property {string} openingHours
- * @property {number} typeId 
- * @property {string} type 
- * @property {number} latitude 
- * @property {number} longitude 
- * @property {string} photo
- * @property {string} website
- */
+* @typedef {Object} Place
+* @property {number} id
+* @property {string} name 
+* @property {string} address
+* @property {string} city 
+* @property {string} postcode 
+* @property {string} phone
+* @property {string} email
+* @property {string} website
+* @property {string} openingHours
+* @property {number} typeId 
+* @property {string} type 
+* @property {string} description
+* @property {string} photo
+* @property {string} facilities_image
+* @property {number} rating
+* @property {number} latitude 
+* @property {number} longitude 
+* @property {string} createdAt
+* @property {string} updatedAt
+* @property {string} deletedAt
+*/
