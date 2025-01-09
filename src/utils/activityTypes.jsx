bd2fa39f74dd7f1d72a09426a6ac66a46c3e5a36
laurawarren88 <laura@enterpriseautomation.co.uk>
2025-01-09 10:59:19 +0000

    Moved activity Types to reusable location

---
+++ src/utils/activityTypes.jsx
@@ -0,0 +1,32 @@
+const activityTypes = [
+
+    'Basketball Court', 
+    'Boxing Gym',
+    'CrossFit Box', 
+    'Cycling Route', 
+    'Cycling Studio', 
+    'Dance Studio', 
+    'Fitness Bootcamp',
+    'Golf Course',
+    'Gym', 
+    'Health Club', 
+    'Hiking Trail',
+    'Horse Riding Centre', 
+    'Leisure Centre', 
+    'Martial Art',
+    'Meditation Centre',
+    'Personal Trainer', 
+    'Pilates Studio', 
+    'Rock Climbing Centre', 
+    'Rowing Centre', 
+    'Running Track', 
+    'Skiing/Snowboarding Centre', 
+    'Spa and Wellness Centre', 
+    'Sports Club',
+    'Swimming Pool', 
+    'Tennis Court', 
+    'Yoga Studio', 
+
+];
+
+export default activityTypes;
